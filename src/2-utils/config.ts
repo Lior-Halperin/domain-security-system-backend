674667b1e5bb406360f2b1093136647a743bc14c
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2023-09-11 10:56:18 +0300

    Added env declatation file, and config.ts file.

---
+++ src/2-utils/config.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+type envOperatorType = "_DEV" | "_TEST" | "_PROD";
+
+const EnvOperator = ():envOperatorType => {
+    switch (process.env.NODE_ENV) {
+      case "development":
+        return "_DEV";
+  
+      case "testing":
+        return "_TEST";
+  
+      case "production":
+        return "_PROD";
+  
+      default:
+        return "_DEV";
+    }
+  };
+  
+const DbConfig = (operator: envOperatorType) => {
+  return {
+    type: process.env[`DB_TYPE${operator}`],
+    host: process.env[`DB_HOST${operator}`],
+    port: process.env[`DB_PORT${operator}`],
+    username: process.env[`DB_USER${operator}`],
+    database: process.env[`DB_NAME${operator}`],
+    password: process.env[`DB_PASSWORD${operator}`],
+  }
+};
+
+const envOperator = EnvOperator()
+const serverPort = process.env[`SERVER_PORT${envOperator}`]
+const dbConfig = DbConfig(envOperator)
+
+export default {
+    dbConfig,
+    serverPort
+};
