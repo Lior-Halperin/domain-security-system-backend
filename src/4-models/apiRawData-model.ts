2dd5a3fa1001f4b51dca46c22fe0e90f53b0339a
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2023-09-12 11:53:54 +0300

    Edited apiRawData-model.ts file

--- src/4-models/apiRawData-model.ts
+++ src/4-models/apiRawData-model.ts
@@ -1,6 +1,7 @@
-import ApisList from "./apis-list"
+import ApisList from "./apis-list";
 
 export interface IApiRawDataModel {
-    rawData: object
-    apiType: ApisList
+  rawData: object;
+  apiType: ApisList;
+  hashSelectedData: string;
 }
