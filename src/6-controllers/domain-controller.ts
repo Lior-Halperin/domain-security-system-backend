8ade6242f93db8f3b89a33ca45239489b08ddf8c
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2023-09-13 11:23:20 +0300

    Add new controller for add domain.

--- src/6-controllers/domain-controller.ts
+++ src/6-controllers/domain-controller.ts
@@ -18,8 +18,20 @@ router.get("/domain/:domainName", async (request: Request, response: Response, n
 
     }
     catch(err:any){
-        console.log(111111)
         next(err)
+    }
+});
+
+// GET http://localhost:3002/api/add-domain
+router.post("/add-domain", async (request: Request, response: Response, next: NextFunction)=>{
+    try{
+        const domainName = request.body.domain
+        const messageResponse: string = await logic.addNewDomain(domainName)
+
+        response.status(201).send(messageResponse)
+    }
+    catch(err:any){
+         next(err)
     }
 });
 
