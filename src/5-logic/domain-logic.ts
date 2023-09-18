f63e2a6efd625e413d108b87192ebb26503e809f
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2023-09-18 15:48:40 +0300

    Added queries for add new domain to identity_info and security_info tables.

--- src/5-logic/domain-logic.ts
+++ src/5-logic/domain-logic.ts
@@ -60,9 +60,13 @@ async function addNewDomain(domainName: string): Promise<any>{ // Todo: promise 
 
   // sql query:
   const sql = `INSERT INTO domains(domainName,activityStatus) VALUES(?,?)`;
+  const sql2 = `INSERT INTO identity_info(domainName, status) VALUES(?,?)`;
+  const sql3 = `INSERT INTO security_info(domainId , status) VALUES(?,?)`;
 
   // Send query do DB - add domain
-  await dal.execute(sql, [domainName]);
+  await dal.execute(sql, [domainName,'active']);
+  await dal.execute(sql2, [domainName,'pending']);
+  await dal.execute(sql3, [domainName,'pending']);
 
   return "The domain has been accepted and is waiting to be scanned.";
 }
