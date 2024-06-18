09791da053bbf6dc2e821612a4b878fe62473a51
Lior-Halperin <98975628+Lior-Halperin@users.noreply.github.com>
2024-06-18 15:12:29 +0300

    Editing the x function so that all API calls occur simultaneously and according th the delay required by the API

--- src/5-logic/scanDomains-logic.ts
+++ src/5-logic/scanDomains-logic.ts
@@ -1,3 +1,4 @@
+import { response } from "express";
 import dbDal from "../2-utils/db-dal";
 import ApiRequestModel from "../4-models/apiRequest-model";
 import { ApisList } from "../4-models/apis-list";
@@ -7,38 +8,61 @@ async function getDomainsByLastUpdateDate(apiName: string): Promise<any> {
   //Todo: the promise type need to be domainModel[]
 
   // ASC - With this change, the domains that were scanned longest ago (over a month ago) will appear first, while those that were scanned more recently (but still over a month ago) will appear last.
-//   const sql =
-    // "SELECT * FROM domains WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR activityStatus = 'new' ORDER BY scanDate ASC";
-    let sql: string;
-    switch (apiName){
-        case (ApisList.whois):
-        sql = "SELECT domainName FROM identity_info WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR scanDate = 'pending' ORDER BY scanDate ASC";
-        break;
-        case (ApisList.virusTotal):
-        sql = "SELECT domainId AS domainName FROM security_info WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR scanDate = 'pending' ORDER BY scanDate ASC";
-        break;
-    }
+  //   const sql =
+  // "SELECT * FROM domains WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR activityStatus = 'new' ORDER BY scanDate ASC";
+  let sql: string;
+  switch (apiName) {
+    case ApisList.whois:
+      sql =
+        "SELECT domainName FROM identity_info WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR scanDate = 'pending' ORDER BY scanDate ASC";
+      break;
+    case ApisList.virusTotal:
+      sql =
+        "SELECT domainId AS domainName FROM security_info WHERE DATEDIFF(CURDATE(), scanDate) > 30 OR scanDate = 'pending' ORDER BY scanDate ASC";
+      break;
+  }
   const domains = await dbDal.execute(sql);
-console.log(domains)
+  console.log(domains);
   return domains;
 }
 
 // Function that sends API requests every x milliseconds for each domain until the list is exhausted
-function processDomainsWithInterval(domains: string[],apiRequestModel: ApiRequestModel) {
-  let index = 0;
-  
-    const intervalDelay:number = apiRequestModel.duration/apiRequestModel.limitRequest
-  // Runs the function that brings information from the external API as the number of domains I received, in a time interval according to the variable I received
-  const intervalId = setInterval(() => {
-    if (index < domains.length) {
-    //   sendRequestAndUpdateDB(domains[index]).catch((error) =>
-    apiRequestModel.axiosRequest(domains[index])        
-    //   ); // Todo: Document the errors.
-      index++;
+async function processDomainsWithInterval(domains: string[],apiRequestModel: ApiRequestModel): Promise<void> {
+  // Calculate the delay interval
+  const intervalDelay: number =
+    apiRequestModel.duration / apiRequestModel.limitRequest;
+
+  const promises: Promise<any>[] = [];
+
+  // Run the function that brings information from the external API for each domain
+  for (let index = 0; index < domains.length; index++) {
+    // Create a promise for the delay
+    const delayPromise = new Promise((resolve) =>
+      setTimeout(resolve, intervalDelay)
+    );
+
+    // Chain the API request to the delay promise
+    const requestPromise = delayPromise.then(() =>
+      apiRequestModel
+        .axiosRequest(domains[index])
+        .then((response) => ({ status: "fulfilled", value: response }))
+        .catch((error) => ({ status: "rejected", reason: error }))
+    );
+    // Add the chained promise to the array
+    promises.push(requestPromise);
+  }
+
+  // Use Promise.allSettled to send API calls concurrently and handel all outcomes
+  const results = await Promise.allSettled(promises);
+
+  // Process the results
+  results.forEach((result, idx) => {
+    if (result.status === "fulfilled") {
+      console.log(`Response from ${domains[idx]}:`, result.value);
     } else {
-      clearInterval(intervalId); // clear interval once all domains are processed
+      console.error(`Error from ${domains[idx]}:`, result.reason);
     }
-  }, intervalDelay);
+  });
 }
 
 export default {
